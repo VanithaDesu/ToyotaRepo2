19e24339b7ee9d248b87e99c83c445ae0e43bcf7
desu.vanitha <desuvanitha1234@gmail.com>
2022-08-01 14:25:16 +0530

    Add a Method

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,5 +1,12 @@
 package com.example.controller;
 
-public class DemoController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class DemoController {
+	@RequestMapping(value ="/")
+public String method1() {
+	return("Hi");
+}
 }
